5401b0436d7bf5edf46408bb2594d96b8b6b581c
Christian Ponte <christian.ponte@udc.es>
2020-04-21 01:42:31 +0200

    Disable Jekyll development mode

--- scripts/cibuild.sh
+++ scripts/cibuild.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
+export JEKYLL_ENV=production
+
 bundle exec jekyll build
 bundle exec htmlproofer --internal-domains "chponte.github.io" ./_site
 touch ./_site/.nojekyll
